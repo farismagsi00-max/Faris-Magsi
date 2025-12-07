de10e3d1f588f19fcf2ce69a6e90375b68a5a961
farismagsi00-max <farismagsi00@gmail.com>
2025-12-07 23:28:54 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import DetailScreen from "./screens/DetailScreen";
+import AddExerciseScreen from "./screens/AddExerciseScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+        <Stack.Screen name="Add Exercise" component={AddExerciseScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+
+
+This app is my assignment for Mobile Application Development subject.
+I created it using Expo CLI and React Native. I used VS Code to write the code.
+
+- Shows a list of exercises
+- When clicking an exercise, it shows details like picture and description
+- User can add new exercise using a form
+
+
+1. Install Node.js and Expo CLI
+2. Download or clone this project
+3. Run: npm install
+4. Then run: npx expo start
+
+Submitted by: Sadaqat
